5842a2a9a6330e7e0117d13b5828470f5e1c5faf
whalewalker <mongodbms@gmail.com>
2022-04-14 05:32:50 +0100

    Connect api to talk to dev service

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-import {API_BASE_URL_LOCAL} from "../constants";
+import {API_BASE_URL_LOCAL, API_BASE_URL_DEV} from "../constants";
 
 export const api = axios.create({
-    baseURL: API_BASE_URL_LOCAL,
+    baseURL: API_BASE_URL_DEV,
 })
