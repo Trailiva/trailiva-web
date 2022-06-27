2acff42367cc5eb37ba35e73ff05f890b2bcbb49
Jamiu Muritala <muritalajamiu2000@gmail.com>
2022-06-27 14:27:04 +0100

    Trail 110 create button component (#8)
    * change to dev url
    
    * TRAIL-84: finished with redesign of verification page
    
    * TRAIL-84: added page responsiveness
    
    * TRAIL-84: commented unused variable
    
    * TRAIL-84: adjusted design to match description
    
    * TRAIL-84: adjusted design to meet specifications
    
    * TRAIL-110: finished with component build
    
    * TRAIL-110: added component styles
    
    * TRAIL-110: completed component styles of button with and without icon
    
    * TRAIL-110: completed component styles of icon button
    
    * TRAIL-110: striped out dev route for rendering buttons and removed unused imports
    
    Co-authored-by: whalewalker <mongodbms@gmail.com>

--- src/App.js
+++ src/App.js
@@ -14,39 +14,39 @@ import CreateWorkspace from "./pages/workspace/personal/CreateWorkspace";
 
 
 function App() {
-    return (
-        <>
-            <Routes>
-            <Route exact path='/' element={
-            <ProtectedRoute>
-                <Dashboard/>
-            </ProtectedRoute>
-        }/>
-            <Route exact path="login" element={<Login/>}/>
-            <Route path="create-workspace" element={
-                <ProtectedRoute>
-                    <CreateWorkspace/>
-                </ProtectedRoute>
-            }/>
-            <Route path="user-verification/:token" element={
-                <ProtectedRoute>
-                    <Verification/>
-                </ProtectedRoute>
-            }/>
+	return (
+		<>
+			<Routes>
+				<Route exact path='/' element={
+					<ProtectedRoute>
+						<Dashboard/>
+					</ProtectedRoute>
+				}/>
+				<Route exact path="login" element={<Login/>}/>
+				<Route path="create-workspace" element={
+					<ProtectedRoute>
+						<CreateWorkspace/>
+					</ProtectedRoute>
+				}/>
+				<Route path="user-verification/:token" element={
+					<ProtectedRoute>
+						<Verification/>
+					</ProtectedRoute>
+				}/>
 
-            <Route path="register" element={<Register/>}/>
-            <Route exact path="to-verify" element={<ToVerify/>}/>
+				<Route path="register" element={<Register/>}/>
+				<Route exact path="to-verify" element={<ToVerify/>}/>
 
-            <Route path="forget-password" element={<ForgetPassword/>}/>
-            <Route path="reset-password" element={
-                <ProtectedRoute>
-                    <ResetPassword/>
-                </ProtectedRoute>
-            }/>
-            <Route exact path="*" element={<PageNotFound/>}/>
-            </Routes>
-            <ToastContainer/>
-        </>);
+				<Route path="forget-password" element={<ForgetPassword/>}/>
+				<Route path="reset-password" element={
+					<ProtectedRoute>
+						<ResetPassword/>
+					</ProtectedRoute>
+				}/>
+				<Route exact path="*" element={<PageNotFound/>}/>
+			</Routes>
+			<ToastContainer/>
+		</>);
 }
 
 export default App;
--- src/pages/auth/verificationPage/ToVerify.js
+++ src/pages/auth/verificationPage/ToVerify.js
@@ -1,43 +1,88 @@
 import React, {useState} from 'react';
 import Navbar from "../../../components/Navbar";
-import VerificationIcon from "../../../images/verify.svg";
-import ResendIcon from "../../../images/App.test.svg"
-import {useNavigate} from "react-router-dom";
-import {handleResetVerificationToken} from "../../../services/authService";
-
+import {AlertTitle, Alert} from "@mui/lab";
+import {makeStyles} from "@material-ui/core/styles";
+import {CheckRounded} from "@mui/icons-material";
+import {Snackbar} from "@mui/material";
 
 const ToVerify = () => {
-    const [message, setMessage] = useState("Your account has been created successfully.  A verification\n" +
-        "link has been sent to your registered email address.");
-    const [icon, setIcon] = useState(VerificationIcon);
-    const navigate = useNavigate();
+	const snackbar = makeStyles({
+		snackbar: {
+			"&.MuiSnackbar-root": {
+				position: "absolute",
+				// transform: "translateX(0)"
+			}
+		}
+	})
+	const useStyles = makeStyles({
+		cookieAlert: {
+			"&.MuiPaper-root": {
+				justifyContent: "space-between",
+				width: '90%',
+				height: "auto",
+				fontSize: "12px",
+				alignItems: 'flex-start',
+				padding: '.5rem',
+				background: '#FAFFFD',
+				borderRadius: '12px',
+				border: '1.6px solid #00C271',
+			},
+			"&.MuiAlert-message": {
+				width: "60%",
+				margin: '2rem'
+			},
+			"& .MuiAlert-icon": {
+				fontSize: 20,
+				width: 'max-content',
+				height: "100%",
+				margin: "auto 5px",
+				padding: '20px',
+				marginRight: "15px",
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'flex-start',
+				borderRadius: '50%',
+				background: '#E6FFF4',
+			}
+		}
+	});
 
+	const [open, setOpen] = React.useState(true);
+	const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+		setOpen(false);
+	};
 
-    const handleResendToken = async () => {
-        try {
-            const res = await handleResetVerificationToken();
-            console.log(res.data);
-            setIcon(ResendIcon);
-            setMessage(res.data.message);
-        } catch (err) {
-            console.log(err.request);
-            console.log(err.response);
-            navigate("/login")
-        }
-    }
+	const classes = useStyles()
+	const snackClasses = snackbar()
+	localStorage.setItem('username', 'Mona')
+	const [username, setUsername] = useState(localStorage.getItem('username'))
 
-    return (
-        <>
-            <Navbar path="/login" text="Login"/>
-            <header className="verification_header">
-                <img src={icon} alt="verification-icon"/>
-                <h3>Check your Email to Verify
-                    <br/> your Account</h3>
-                <p>{message}</p>
-                <button className="token-btn" type="button" onClick={handleResendToken}>Resend Token</button>
-            </header>
-        </>
-    );
+	return (
+		<>
+			<Navbar path="/login" text="Login"/>
+			<header className="verification_header">
+				<div>
+					<h3>Verify Your Account</h3>
+					<p>Your account has been created successfully. A <br/> verification link has been sent to your
+						registered email <br/> address.</p>
+				</div>
+
+				<Snackbar
+					className={snackClasses.snackbar}
+					open={open}
+					autoHideDuration={6000}
+					anchorOrigin={{horizontal: "center", vertical: "bottom"}}
+					onClose={handleClose}
+				>
+					<Alert severity="success" onClose={handleClose} icon={<CheckRounded fontSize={"inherit"}/>}
+						   className={classes.cookieAlert}>
+						<AlertTitle>Hello {username}</AlertTitle>
+						You would be sent a recovery Link to the email
+					</Alert>
+				</Snackbar>
+			</header>
+		</>
+	);
 };
 
 export default ToVerify;
