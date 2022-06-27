9a9ddbf141237362d2d01c812cf584a575e5e486
whalewalker <mongodbms@gmail.com>
2022-06-27 20:31:47 +0100

    implement reusable thumnail component for profile image

--- src/App.js
+++ src/App.js
@@ -1,64 +1,54 @@
 import React from 'react';
-import {ToastContainer} from "react-toastify";
+import {Routes, Route} from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
-import Dashboard from "../src/pages/dashboard/Dashboard";
-import {Route, Routes} from "react-router-dom";
+import {Dashboard} from "@material-ui/icons";
 import Verification from "./pages/auth/verificationPage/Verification";
-import Register from "./pages/auth/register/Register";
+import CreateWorkspace from "./pages/workspace/personal/CreateWorkspace";
 import ToVerify from "./pages/auth/verificationPage/ToVerify";
 import ForgetPassword from "./pages/auth/forgetPassword/ForgetPassword";
+import Register from "./pages/auth/register/Register";
 import ResetPassword from "./pages/auth/ResetPassword/ResetPassword";
 import PageNotFound from "./components/PageNotFound";
-import Login from "./pages/auth/login/Login";
-import CreateWorkspace from "./pages/workspace/personal/CreateWorkspace";
-import IsActiveThumbnail from "./components/IsActiveThumbnail";
-import IsAdminThumbnail from "./components/IsAdminThumbnail";
-import thumb from "./images/thumb.svg";
-import mark from "./images/mark.svg";
-import cancel from "./images/cancel.svg";
-
+import {ToastContainer} from "react-toastify";
+import {Login} from "@mui/icons-material";
 
 
 function App() {
-    const handleClick = () => {
-        console.log("Clicked")
-    }
     return (
-        <div style={{background: "blanchedalmond", width: "100vw", height: "100vh", padding: "1rem", margin: 0}}>
-        {/*    <Routes>*/}
-        {/*    <Route exact path='/' element={*/}
-        {/*    <ProtectedRoute>*/}
-        {/*        <Dashboard/>*/}
-        {/*    </ProtectedRoute>*/}
-        {/*}/>*/}
-        {/*    <Route exact path="login" element={<Login/>}/>*/}
-        {/*    <Route path="create-workspace" element={*/}
-        {/*        <ProtectedRoute>*/}
-        {/*            <CreateWorkspace/>*/}
-        {/*        </ProtectedRoute>*/}
-        {/*    }/>*/}
-        {/*    <Route path="user-verification/:token" element={*/}
-        {/*        <ProtectedRoute>*/}
-        {/*            <Verification/>*/}
-        {/*        </ProtectedRoute>*/}
-        {/*    }/>*/}
+        <>
+            <Routes>
+                <Route exact path='/' element={
+                    <ProtectedRoute>
+                        <Dashboard/>
+                    </ProtectedRoute>
+                }/>
+                <Route exact path="login" element={<Login/>}/>
+                <Route path="create-workspace" element={
+                    <ProtectedRoute>
+                        <CreateWorkspace/>
+                    </ProtectedRoute>
+                }/>
+                <Route path="user-verification/:token" element={
+                    <ProtectedRoute>
+                        <Verification/>
+                    </ProtectedRoute>
+                }/>
 
-        {/*    <Route path="register" element={<Register/>}/>*/}
-        {/*    <Route exact path="to-verify" element={<ToVerify/>}/>*/}
+                <Route path="register" element={<Register/>}/>
+                <Route exact path="to-verify" element={<ToVerify/>}/>
 
-        {/*    <Route path="forget-password" element={<ForgetPassword/>}/>*/}
-        {/*    <Route path="reset-password" element={*/}
-        {/*        <ProtectedRoute>*/}
-        {/*            <ResetPassword/>*/}
-        {/*        </ProtectedRoute>*/}
-        {/*    }/>*/}
-        {/*    <Route exact path="*" element={<PageNotFound/>}/>*/}
-        {/*    </Routes>*/}
-        {/*    <ToastContainer/>*/}
-            <IsAdminThumbnail icon={mark} image={thumb}/>
-            <IsAdminThumbnail icon={cancel} image={thumb}/>
-            <IsActiveThumbnail image={thumb}/>
-        </div>);
+                <Route path="forget-password" element={<ForgetPassword/>}/>
+                <Route path="reset-password" element={
+                    <ProtectedRoute>
+                        <ResetPassword/>
+                    </ProtectedRoute>
+                }/>
+                <Route exact path="*" element={<PageNotFound/>}/>
+            </Routes>
+            <ToastContainer/>
+
+
+        </>);
 }
 
 export default App;
