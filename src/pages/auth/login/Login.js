caac30e63cab72d7b02aa55309703a66d2356d1e
yjcode <favourejim56@gmail.com>
2022-07-07 20:24:30 +0100

    laptop view created

--- src/pages/auth/login/Login.js
+++ src/pages/auth/login/Login.js
@@ -1,117 +1,115 @@
 import Navbar from "../../../components/Navbar";
-import {registrationOption} from "../../../utils/formValidation";
-import {useForm} from "react-hook-form";
-import {ACCESS_TOKEN, HAS_WORKSPACE, TOKEN_EXPIRY_DATE, VERIFICATION_TOKEN,} from "../../../constants";
-import FormControl from "../../../components/FormControl";
+import { registrationOption } from "../../../utils/formValidation";
+import { useForm } from "react-hook-form";
+import {
+  ACCESS_TOKEN,
+  HAS_WORKSPACE,
+  TOKEN_EXPIRY_DATE,
+  VERIFICATION_TOKEN,
+} from "../../../constants";
 import AuthButton from "../../../components/AuthButton";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
-import {toast} from "react-toastify";
-import {useState} from "react";
-import {extractErrorMessage} from "../../../utils/helper";
-import {handleForgetPasswordToken, handleUserLogin,} from "../../../services/authService";
-import Link from '@mui/material/Link';
+import { toast } from "react-toastify";
+import { useState } from "react";
+import { extractErrorMessage } from "../../../utils/helper";
+import {
+  handleForgetPasswordToken,
+  handleUserLogin,
+} from "../../../services/authService";
+import Link from "@mui/material/Link";
 import Box from "@mui/material/Box";
-
+import IsInputComponent from "../../../components/InputFields/IsInputComponent";
 
 const Login = () => {
-    const {
-        register,
-        handleSubmit,
-        reset,
-        formState: {errors},
-    } = useForm();
-    const navigate = useNavigate();
-    const [loading, setLoading] = useState(false);
+  const { control, handleSubmit, reset } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
-    const login = async (data) => {
-        setLoading(true);
-        try {
-            const res = await handleUserLogin(data);
-            localStorage.setItem(ACCESS_TOKEN, res.data.jwtToken);
-            setLoading(false);
-            if (!localStorage.getItem(HAS_WORKSPACE)) navigate("/create-workspace");
-            else navigate("/");
-        } catch (error) {
-            setLoading(false);
-            const message = extractErrorMessage(error);
-            toast.error(message);
-        }
-        reset({
-            email: "",
-            password: "",
-        });
-    };
+  const login = async (data) => {
+    setLoading(true);
+    try {
+      const res = await handleUserLogin(data);
+      localStorage.setItem(ACCESS_TOKEN, res.data.jwtToken);
+      setLoading(false);
+      if (!localStorage.getItem(HAS_WORKSPACE)) navigate("/create-workspace");
+      else navigate("/");
+    } catch (error) {
+      setLoading(false);
+      const message = extractErrorMessage(error);
+      toast.error(message);
+    }
+    reset({
+      email: "",
+      password: "",
+    });
+  };
 
-    const handleError = (errors) => console.log(errors);
+  const handleError = (errors) => console.log(errors);
 
-    const forgetPasswordHandler = async () => {
-        try {
-            let response = await handleForgetPasswordToken();
-            localStorage.setItem(VERIFICATION_TOKEN, response.data.token);
-            localStorage.setItem(TOKEN_EXPIRY_DATE, response.data.expiry);
-            navigate("/forget-password");
-        } catch (err) {
-            const message = extractErrorMessage(err);
-            toast.error(message);
-            console.log("error ==> ", err);
-        }
-    };
+  const forgetPasswordHandler = async () => {
+    try {
+      let response = await handleForgetPasswordToken();
+      localStorage.setItem(VERIFICATION_TOKEN, response.data.token);
+      localStorage.setItem(TOKEN_EXPIRY_DATE, response.data.expiry);
+      navigate("/forget-password");
+    } catch (err) {
+      const message = extractErrorMessage(err);
+      toast.error(message);
+      console.log("error ==> ", err);
+    }
+  };
 
-    return (
-        <>
-            <Navbar text="Create Account" path="/register"/>
-            <div className="form-container">
-              <Box
-                  variant="body1"
-              >
-                  <div className="form-header">
-                      <h2>Welcome Back Login</h2>
-                  </div>
+  return (
+    <>
+      <Navbar text="Create Account" path="/register" />
+      <div className="form-container">
+        <Box variant="body1">
+          <div className="form-header">
+            <h2>Welcome Back Login</h2>
+          </div>
 
-                  <form onSubmit={handleSubmit(login, handleError)} noValidate>
-                      <FormControl
-                          label="Enter email address"
-                          name="email"
-                          placeholder="example@gmail.com"
-                          visibility={false}
-                          useForm_register_return={register(
-                              "email",
-                              registrationOption.email
-                          )}
-                          errors={errors}
-                      />
+          <form onSubmit={handleSubmit(login, handleError)} noValidate>
+            <IsInputComponent
+              label="Enter email address"
+              name="email"
+              type={"text"}
+              control={control}
+              placeholder="example@gmail.com"
+              validation={registrationOption.email}
+            />
 
-                      <FormControl
-                          label="Enter a password"
-                          name="password"
-                          placeholder="Enter your password"
-                          visibility={true}
-                          useForm_register_return={register(
-                              "password",
-                              registrationOption.password
-                          )}
-                          errors={errors}
-                      />
+            <IsInputComponent
+              label="Enter a password"
+              name="password"
+              type={"password"}
+              control={control}
+              placeholder="Enter your password"
+              validation={registrationOption.password}
+            />
 
-                      <AuthButton
-                          disabled={loading}
-                          text="Login"
-                          loadingText="Logging..."
-                      />
-
-                  </form>
-                  <Link
-                      underline="hover"
-                      sx={{color: "#3754DB", fontSize: "1rem"}}
-                      onClick={forgetPasswordHandler}
-                  >
-                      Forget password ?
-                  </Link>
-              </Box>
-            </div>
-        </>
-    );
+            <AuthButton
+              disabled={loading}
+              text="Login"
+              loadingText="Logging..."
+            />
+          </form>
+          <Link
+            underline="hover"
+            sx={{ color: "#3754DB", fontSize: "1rem" }}
+            onClick={forgetPasswordHandler}
+          >
+            Forget password ?
+          </Link>
+        </Box>
+      </div>
+    </>
+  );
 };
 
 export default Login;
--- src/pages/auth/register/Register.js
+++ src/pages/auth/register/Register.js
@@ -1,7 +1,6 @@
 import "./register.css";
 import Navbar from "../../../components/Navbar";
 import { registrationOption } from "../../../utils/formValidation";
-import FormControl from "../../../components/FormControl";
 import AuthButton from "../../../components/AuthButton";
 import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
@@ -10,15 +9,20 @@ import { useState } from "react";
 import { extractErrorMessage } from "../../../utils/helper";
 import { toast } from "react-toastify";
 import { handleUserRegistration } from "../../../services/authService";
-import Box from "@mui/material/Box";
+import { Box } from "@mui/material";
+import IsInputComponent from "../../../components/InputFields/IsInputComponent";
+import { Link } from "react-router-dom";
+import logo from "../../../images/Trailiva name logo.svg";
 
 const Register = () => {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm();
+  const { control, handleSubmit, reset } = useForm({
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+    },
+  });
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
 
@@ -45,72 +49,71 @@ const Register = () => {
   const handleError = (errors) => console.log(errors);
 
   return (
-    <>
-      <Navbar text="Login" path="/login" />
-      <div className="form-container">
-       <Box variant="body1">
-         <div className="form-header">
-           <h2>Create an Account</h2>
-           <p>It's Simple and Easy !!</p>
-         </div>
-         <form onSubmit={handleSubmit(registerUser, handleError)} noValidate>
-           <FormControl
-               label="Enter your first name"
-               name="firstName"
-               placeholder="John"
-               visibility={false}
-               useForm_register_return={register(
-                   "firstName",
-                   registrationOption.fullName
-               )}
-               errors={errors}
-           />
-
-           <FormControl
-               label="Enter your last name"
-               name="lastName"
-               placeholder="Doe"
-               visibility={false}
-               useForm_register_return={register(
-                   "lastName",
-                   registrationOption.fullName
-               )}
-               errors={errors}
-           />
-
-           <FormControl
-               label="Enter email address"
-               name="email"
-               placeholder="example@gmail.com"
-               visibility={false}
-               useForm_register_return={register(
-                   "email",
-                   registrationOption.email
-               )}
-               errors={errors}
-           />
-
-           <FormControl
-               label="Enter a password"
-               name="password"
-               placeholder="Enter your password"
-               visibility={true}
-               useForm_register_return={register(
-                   "password",
-                   registrationOption.password
-               )}
-               errors={errors}
-           />
-
-           <AuthButton
-               disabled={loading}
-               text="Create Account"
-               loadingText="Loading..."
-           />
-         </form>
-       </Box>
+    <div className={"create-acc-container"}>
+      <div className={"bx-1"}>
+        <div>
+          <Link to="/" className="logo-img">
+            <img src={logo} alt="logo" />
+          </Link>
+        </div>
+        <div className="title">Take your productivity to the next level.</div>
+        <div className="copyright">Copyright 2022 | All Rights Reserved</div>
       </div>
-    </>
+      <div className={"bx-2"}>
+        <Navbar text="Login" path="/login" />
+        <div className="form-container">
+          <Box variant="body1">
+            <div className="form-header">
+              <h2>Create an Account</h2>
+              <p>It's Simple and Easy !!</p>
+            </div>
+            <form onSubmit={handleSubmit(registerUser, handleError)} noValidate>
+              <IsInputComponent
+                label="Enter your first name"
+                name="firstName"
+                type={"text"}
+                control={control}
+                placeholder="John"
+                validation={registrationOption.fullName}
+              />
+
+              <IsInputComponent
+                label="Enter your last name"
+                name="lastName"
+                type={"text"}
+                control={control}
+                placeholder="Doe"
+                validation={registrationOption.fullName}
+              />
+
+              <IsInputComponent
+                label="Enter email address"
+                name="email"
+                type={"text"}
+                control={control}
+                placeholder="example@gmail.com"
+                validation={registrationOption.email}
+              />
+
+              <IsInputComponent
+                label="Enter a password"
+                type={"password"}
+                name="password"
+                control={control}
+                placeholder="Enter your password"
+                validation={registrationOption.password}
+              />
+
+              <AuthButton
+                disabled={loading}
+                text="Create Account"
+                loadingText="Loading..."
+              />
+            </form>
+          </Box>
+        </div>
+      </div>
+    </div>
   );
 };
 
