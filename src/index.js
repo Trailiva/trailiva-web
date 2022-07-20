436bc3cbab0199f9af5e435b4ea1fe4ecbd273f2
Ejim Favour <favourejim56@gmail.com>
2022-07-20 08:14:15 +0100

    TRAIL-91: revamp-auth-service-to-redux (#18)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./store/index";
 
 ReactDOM.render(
-    <BrowserRouter>
-        <React.StrictMode>
-                <App/>
-        </React.StrictMode>
-    </BrowserRouter>
-    , document.getElementById('root')
+  <BrowserRouter>
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/auth/ResetPassword/ResetPassword.js
+++ src/pages/auth/ResetPassword/ResetPassword.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import Navbar from "../../../components/Navbar";
 import { Alert } from "@mui/material";
 import FormControl from "../../../components/FormControl";
@@ -6,40 +6,52 @@ import { registrationOption } from "../../../utils/formValidation";
 import AuthButton from "../../../components/AuthButton";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
-import { handleResetPassword } from "../../../services/authService";
 import Box from "@mui/material/Box";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useDispatch, useSelector } from "react-redux";
+import { authAction } from "../../../store/auth-slice";
+import { resetPassword } from "../../../store/auth-actions";
 
 const ResetPassword = () => {
+  const dispatchFn = useDispatch();
+  const loading = useSelector((state) => state.auth.isLoading);
+  const errorMessage = useSelector((state) => state.auth.errorMsg);
+  const isSuccessful = useSelector((state) => state.auth.isSuccessful);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (errorMessage) {
+      toast.error(errorMessage);
+    } else if (isSuccessful) {
+      toast.success(isSuccessful);
+    }
+
+    return () => {
+      setTimeout(() => {
+        dispatchFn(authAction.setErrorMsg(""));
+        dispatchFn(authAction.setSuccessMsg(""));
+      }, 5000);
+    };
+  }, [errorMessage, dispatchFn, isSuccessful]);
+
   const {
     register,
     handleSubmit,
     reset,
     formState: { errors },
   } = useForm();
-  const [isSuccessFul, setIsSuccessFul] = useState(true);
-  const [loading, setLoading] = useState(false);
-  const [errorDate, setErrorData] = useState("");
-  const navigate = useNavigate();
 
-  const resetPassword = async (data) => {
-    setLoading(true);
-    try {
-      const res = await handleResetPassword(data);
-      setLoading(false);
-      setIsSuccessFul(res.data.successful);
+  const resetPasswordHandler = async (data) => {
+    const navigateToLoginHandler = () => {
+      reset({
+        email: "",
+        oldPassword: "",
+        password: "",
+      });
       navigate("/loginHandler");
-    } catch (err) {
-      setLoading(false);
-      setIsSuccessFul(false);
-      setErrorData(err);
-      console.log("err", err);
-      navigate("/loginHandler");
-    }
-    reset({
-      email: "",
-      oldPassword: "",
-      password: "",
-    });
+    };
+    dispatchFn(resetPassword(data, navigateToLoginHandler));
   };
 
   const handleError = (errors) => console.log(errors);
@@ -48,64 +60,67 @@ const ResetPassword = () => {
     <>
       <Navbar path="/register" text="Create Account" />
       <div className="form-container">
-      <Box variant="bod1">
-        {!isSuccessFul && (
+        <Box variant="bod1">
+          {errorMessage && (
             <Alert
-                variant="filled"
-                severity="error"
-                style={{ marginBottom: "1rem" }}
+              variant="filled"
+              severity="error"
+              style={{ marginBottom: "1rem" }}
             >
-              {errorDate}!
+              {errorMessage}!
             </Alert>
-        )}
-        <div className="form-header">
-          <h2>Reset Password</h2>
-        </div>
+          )}
+          <div className="form-header">
+            <h2>Reset Password</h2>
+          </div>
 
-        <form onSubmit={handleSubmit(resetPassword, handleError)} noValidate>
-          <FormControl
+          <form
+            onSubmit={handleSubmit(resetPasswordHandler, handleError)}
+            noValidate
+          >
+            <FormControl
               label="Enter email address"
               name="email"
               placeholder="example@gmail.com"
               visibility={false}
               useForm_register_return={register(
-                  "email",
-                  registrationOption.email
+                "email",
+                registrationOption.email
               )}
               errors={errors}
-          />
+            />
 
-          <FormControl
+            <FormControl
               label="Enter your old password"
               name="password"
               placeholder="Enter your password"
               visibility={true}
               useForm_register_return={register(
-                  "oldPassword",
-                  registrationOption.password
+                "oldPassword",
+                registrationOption.password
               )}
               errors={errors}
-          />
+            />
 
-          <FormControl
+            <FormControl
               label="Enter a your new password"
               name="password"
               placeholder="Enter your password"
               visibility={true}
               useForm_register_return={register(
-                  "password",
-                  registrationOption.password
+                "password",
+                registrationOption.password
               )}
               errors={errors}
-          />
+            />
 
-          <AuthButton
+            <AuthButton
               disabled={loading}
               text="Reset Password"
               loadingText="Loading..."
-          />
-        </form>
-      </Box>
+            />
+          </form>
+        </Box>
       </div>
     </>
   );
