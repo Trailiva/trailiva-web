c97703e7bd29c9a3dcacd32505ed6860e4c5e77e
Ejim Favour <favourejim56@gmail.com>
2022-09-02 21:54:48 +0100

    Trail 141 redesign authentication pages (#24)
    * eslint resolved
    
    * TRAIL-141: login page edited

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,35 +4,50 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import CustomButton from "./Buttons/CustomButton";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-
-const Navbar = ({text, path}) => {
-    const navigate = useNavigate();
-    return (
-        <AppBar position="static"
-                style={{background: 'transparent', boxShadow: 'none', maxWidth: "1200px", margin: "0.5rem auto"}}>
-            <Toolbar sx={{display: "flex", alignItems: "center", justifyContent: "space-between"}}>
-                <Box
-                    component="img"
-                    sx={{height: 18, cursor: "pointer"}}
-                    alt="Logo"
-                    src={Logo}
-                    onClick={() => {
-                        navigate("/")
-                    }}
-                />
-                <CustomButton
-                    text={{value: text}}
-                    variant={"secondary"}
-                    color={"rgba(55, 84, 219, 1)"}
-                    size={"sm"}
-                    sx={{padding: "0.2rem 1.5rem", textTransform: "capitalize"}}
-                    handleClick={() => {navigate(path)}}
-                />
-            </Toolbar>
-        </AppBar>
-    );
+const Navbar = ({ text, path }) => {
+  const navigate = useNavigate();
+  return (
+    <AppBar
+      position="static"
+      style={{
+        background: "transparent",
+        boxShadow: "none",
+        maxWidth: "1200px",
+        margin: "0 auto",
+      }}
+    >
+      <Toolbar
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          padding: "0",
+        }}
+      >
+        <Box
+          component="img"
+          sx={{ height: 18, cursor: "pointer" }}
+          alt="Logo"
+          src={Logo}
+          onClick={() => {
+            navigate("/");
+          }}
+        />
+        <CustomButton
+          text={{ value: text }}
+          variant={"secondary"}
+          color={"rgba(55, 84, 219, 1)"}
+          size={"sm"}
+          sx={{ padding: "0.4rem 1.5rem", textTransform: "capitalize" }}
+          handleClick={() => {
+            navigate(path);
+          }}
+        />
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Navbar;
