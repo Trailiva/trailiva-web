768a2a635ba43cc01ab4ee34699b495a6347bd77
murytarlah <dknight.jim@gmail.com>
2022-09-04 01:26:29 +0100

    TRAIL-84: finished with task

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {ToastContainer} from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Dashboard from "../src/pages/dashboard/Dashboard";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Verification from "./pages/auth/verificationPage/Verification";
 import Register from "./pages/auth/register/Register";
 import ToVerify from "./pages/auth/verificationPage/ToVerify";
@@ -14,39 +14,39 @@ import CreateWorkspace from "./pages/workspace/personal/CreateWorkspace";
 
 
 function App() {
-    return (
-        <>
-            <Routes>
-            <Route exact path='/' element={
-            <ProtectedRoute>
-                <Dashboard/>
-            </ProtectedRoute>
-        }/>
-            <Route exact path="login" element={<Login/>}/>
-            <Route path="create-workspace" element={
-                <ProtectedRoute>
-                    <CreateWorkspace/>
-                </ProtectedRoute>
-            }/>
-            <Route path="user-verification/:token" element={
-                <ProtectedRoute>
-                    <Verification/>
-                </ProtectedRoute>
-            }/>
+	return (
+		<>
+			<Routes>
+				<Route exact path='/' element={
+					<ProtectedRoute>
+						<Dashboard />
+					</ProtectedRoute>
+				} />
+				<Route exact path="login" element={<Login />} />
+				<Route path="create-workspace" element={
+					<ProtectedRoute>
+						<CreateWorkspace />
+					</ProtectedRoute>
+				} />
+				<Route path="user-verification/:token" element={
+					<ProtectedRoute>
+						<Verification />
+					</ProtectedRoute>
+				} />
 
-            <Route path="register" element={<Register/>}/>
-            <Route exact path="to-verify" element={<ToVerify/>}/>
+				<Route path="register" element={<Register />} />
+				<Route exact path="to-verify" element={<ToVerify />} />
 
-            <Route path="forget-password" element={<ForgetPassword/>}/>
-            <Route path="reset-password" element={
-                <ProtectedRoute>
-                    <ResetPassword/>
-                </ProtectedRoute>
-            }/>
-            <Route exact path="*" element={<PageNotFound/>}/>
-            </Routes>
-            <ToastContainer/>
-        </>);
+				<Route path="forget-password" element={<ForgetPassword />} />
+				<Route path="reset-password" element={
+					<ProtectedRoute>
+						<ResetPassword />
+					</ProtectedRoute>
+				} />
+				<Route exact path="*" element={<PageNotFound />} />
+			</Routes>
+			<ToastContainer />
+		</>);
 }
 
 export default App;
