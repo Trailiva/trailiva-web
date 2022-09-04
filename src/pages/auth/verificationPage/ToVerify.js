918f3fe22a95688d6d579c35066adace3258b530
Jamiu Muritala <dknight.jim@gmail.com>
2022-09-04 09:30:07 +0100

    Trail 84 redesign verify account page (#26)
    * TRAIL-84: finished with redesign of verification page
    
    * TRAIL-84: added page responsiveness
    
    * TRAIL-84: commented unused variable
    
    * TRAIL-84: adjusted design to match description
    
    * TRAIL-84: adjusted design to meet specifications
    
    * TRAIL-84: finished with task
    
    Co-authored-by: Ismail Abdullahi ohida <80593013+whalewalker@users.noreply.github.com>

--- src/pages/auth/verificationPage/ToVerify.js
+++ src/pages/auth/verificationPage/ToVerify.js
@@ -1,85 +1,51 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from "../../../components/Navbar";
-import {AlertTitle, Alert} from "@mui/lab";
-import {makeStyles} from "@material-ui/core/styles";
-import {CheckRounded} from "@mui/icons-material";
-import {Snackbar} from "@mui/material";
+import { CheckRounded } from "@mui/icons-material";
+import { toast } from 'react-toastify'
+
+function VerificationMessage(props) {
+	return (
+		<>
+			<h3 style={{
+				color: '#00C271'
+			}}>Hello {props.username}</h3>
+			<p style={{
+				fontSize: "12px",
+				color: '#017042',
+			}}>You would be sent a recovery Link to the email</p>
+		</>
+	)
+}
 
 const ToVerify = () => {
-	const snackbar = makeStyles({
-		snackbar: {
-			"&.MuiSnackbar-root": {
-				position: "absolute",
-				// transform: "translateX(0)"
-			}
-		}
-	})
-	const useStyles = makeStyles({
-		cookieAlert: {
-			"&.MuiPaper-root": {
-				justifyContent: "space-between",
-				width: '90%',
-				height: "auto",
-				fontSize: "12px",
-				alignItems: 'flex-start',
-				padding: '.5rem',
-				background: '#FAFFFD',
-				borderRadius: '12px',
-				border: '1.6px solid #00C271',
-			},
-			"&.MuiAlert-message": {
-				width: "60%",
-				margin: '2rem'
-			},
-			"& .MuiAlert-icon": {
-				fontSize: 20,
-				width: 'max-content',
-				height: "100%",
-				margin: "auto 5px",
-				padding: '20px',
-				marginRight: "15px",
-				display: 'flex',
-				justifyContent: 'center',
-				alignItems: 'flex-start',
-				borderRadius: '50%',
-				background: '#E6FFF4',
-			}
-		}
-	});
-
-	const [open, setOpen] = React.useState(true);
-	const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
-		setOpen(false);
-	};
-
-	const classes = useStyles()
-	const snackClasses = snackbar()
 	localStorage.setItem('username', 'Mona')
 	const [username, setUsername] = useState(localStorage.getItem('username'))
 
+	const notify = () => {
+		toast(<VerificationMessage username={username} />, {
+			position: 'bottom-center',
+			autoClose: false,
+			icon: <CheckRounded fontSize={"inherit"} />,
+			hideProgressBar: true,
+			closeOnClick: false,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined,
+		})
+	}
+
+	useEffect(() => {
+		notify()
+	}, []);
+
 	return (
 		<>
-			<Navbar path="/login" text="Login"/>
+			<Navbar path="/login" text="Login" />
 			<header className="verification_header">
 				<div>
 					<h3>Verify Your Account</h3>
-					<p>Your account has been created successfully. A <br/> verification link has been sent to your
-						registered email <br/> address.</p>
+					<p>Your account has been created successfully.  A <br /> verification link has been sent to your registered email <br /> address.</p>
 				</div>
-
-				<Snackbar
-					className={snackClasses.snackbar}
-					open={open}
-					autoHideDuration={6000}
-					anchorOrigin={{horizontal: "center", vertical: "bottom"}}
-					onClose={handleClose}
-				>
-					<Alert severity="success" onClose={handleClose} icon={<CheckRounded fontSize={"inherit"}/>}
-						   className={classes.cookieAlert}>
-						<AlertTitle>Hello {username}</AlertTitle>
-						You would be sent a recovery Link to the email
-					</Alert>
-				</Snackbar>
 			</header>
 		</>
 	);
